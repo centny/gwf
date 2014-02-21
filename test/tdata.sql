075a7a29387695b4aa13173f88b5f031a8223222
Centny <centny@gmail.com>
2014-02-21 17:04:22 +0800

    new testing data.

--- test/tdata.sql
+++ test/tdata.sql
@@ -8,10 +8,12 @@ create table TTABLE
    TVAL                 varchar(255),
    STATUS               varchar(255),
    TIME                 timestamp,
+   FVAL					float,
+   UIVAL				int unsigned,
    ADD1                 varchar(255),
    ADD2                 varchar(255),
    primary key (TID)
 );
-INSERT INTO TTABLE (TID,TNAME,TITEM,TVAL,STATUS,TIME) VALUES(1,"测试数据1","测试数据1","abc_1","N",NOW());
-INSERT INTO TTABLE (TID,TNAME,TITEM,TVAL,STATUS,TIME) VALUES(2,"测试数据2","测试数据2","abc_2","N",NOW());
-INSERT INTO TTABLE (TID,TNAME,TITEM,TVAL,STATUS,TIME) VALUES(3,"测试数据3","测试数据3","abc_3","N",NOW());
+INSERT INTO TTABLE (TID,TNAME,TITEM,TVAL,STATUS,TIME) VALUES(1,"测试数据1","测试数据1","abc_1","N",NOW(),3,3);
+INSERT INTO TTABLE (TID,TNAME,TITEM,TVAL,STATUS,TIME) VALUES(2,"测试数据2","测试数据2","abc_2","N",NOW(),3,3);
+INSERT INTO TTABLE (TID,TNAME,TITEM,TVAL,STATUS,TIME) VALUES(3,"测试数据3","测试数据3","abc_3","N",NOW(),3,3);
