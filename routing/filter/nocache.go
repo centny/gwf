84b24f720cde1ea6c2cf23938d999d6d6786f998
Centny <centny@gmail.com>
2015-08-04 10:42:42 +0800

    fix not cache.

--- routing/filter/nocache.go
+++ routing/filter/nocache.go
@@ -6,9 +6,9 @@ import (
 )
 
 func NoCacheFilter(hs *routing.HTTPSession) routing.HResult {
-	hs.R.Header.Set("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
-	hs.R.Header.Set("Last-Modified", time.Now().Format(time.RFC1123))
-	hs.R.Header.Set("Cache-Control", "no-stroe,no-cache,must-revalidate,post-check=0,pre-check=0")
-	hs.R.Header.Set("Pragma", "no-cache")
+	hs.W.Header().Set("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
+	hs.W.Header().Set("Last-Modified", time.Now().Format(time.RFC1123))
+	hs.W.Header().Set("Cache-Control", "no-stroe,no-cache,must-revalidate,post-check=0,pre-check=0")
+	hs.W.Header().Set("Pragma", "no-cache")
 	return routing.HRES_CONTINUE
 }
