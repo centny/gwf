60f6f17243c8fdd33493f55f8760e40f17511dfe
Centny <centny@gmail.com>
2015-08-04 10:44:03 +0800

    adding example cache.

--- example/example.go
+++ example/example.go
@@ -6,6 +6,7 @@ import (
 	"github.com/Centny/gwf/dbutil"
 	"github.com/Centny/gwf/log"
 	"github.com/Centny/gwf/routing"
+	"github.com/Centny/gwf/routing/filter"
 	"github.com/Centny/gwf/test"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
@@ -66,6 +67,50 @@ func main() {
 		hs.W.Write([]byte("some data\n"))
 		return routing.HRES_RETURN
 	})
+	/*
+		json arguments:
+		{
+		  "a":{
+		  	"a1":"val",
+		  	"a2":100
+		  },
+		  "b":{
+			"b1":"val2",
+			"b2":200
+		  }
+		}
+	*/
+	mux.HFunc("^/jsonv(\\?.*)?$", func(hs *routing.HTTPSession) routing.HResult {
+		mv, err := hs.JsonVal("json")
+		if err != nil {
+			return hs.MsgResErr2(1, "arg-err", err)
+		}
+		var a1, b1 string
+		var a2, b2 int64
+		err = mv.ValidF(`
+			a/a1,R|S,L:0;
+			a/a2,R|I,R:0;
+			b/b1,R|S,L:0;
+			b/b2,R|I,R:0;
+			`, &a1, &a2, &b1, &b2)
+		if err != nil {
+			return hs.MsgResErr2(1, "arg-err", err)
+		}
+		fmt.Println(a1, a2, b1, b2)
+		return hs.MsgRes("ok")
+	})
+	var cache1, cache2 int = 0, 0
+	mux.HFunc("^/cache1(\\?.*)?$", func(hs *routing.HTTPSession) routing.HResult {
+		cache1++
+		hs.W.Header().Set("Last-Modified", "Tue, 01 Jan 1980 1:00:00 GMT")
+		return hs.MsgRes(fmt.Sprintf("%v", cache1))
+	})
+	mux.HFilterFunc("^/cache2(\\?.*)?$", filter.NoCacheFilter)
+	mux.HFunc("^/cache2(\\?.*)?$", func(hs *routing.HTTPSession) routing.HResult {
+		cache2++
+		return hs.MsgRes(fmt.Sprintf("%v", cache2))
+	})
+	mux.Handler("^/.*", http.FileServer(http.Dir("www")))
 	s := http.Server{Addr: ":8080", Handler: mux}
 	err = s.ListenAndServe()
 	if err != nil {
