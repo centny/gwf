a040c228bacf4b7eef000e86f87ea7c5304b3d80
Centny <centny@gmail.com>
2016-02-25 18:37:20 +0800

    adding getw command.

---
+++ cmd/getw/getw.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Centny/gwf/util"
+	"net/url"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: getw url <out file>")
+		return
+	}
+	var turl, err = url.Parse(os.Args[1])
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	var fn string
+	if len(os.Args) > 2 {
+		fn = os.Args[2]
+	} else {
+		_, fn = filepath.Split(turl.EscapedPath())
+	}
+	if len(fn) < 1 {
+		fn = "index.html"
+	}
+	fmt.Println("Donwload", os.Args[1], "to", fn)
+	err = util.DLoad(fn, os.Args[1])
+	if err == nil {
+		fmt.Println("OK")
+	} else {
+		fmt.Println(err)
+	}
+}
