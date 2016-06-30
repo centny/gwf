8f543ed0294aebdc31235175eecb518729faa5d6
Centny <centny@gmail.com>
2016-06-30 11:21:58 +0800

    adding check uid.

--- im/do_imc.go
+++ im/do_imc.go
@@ -172,6 +172,9 @@ func (d *DoImc) OnM(i *IMC, c netw.Cmd, m *pb.ImMsg) int {
 	d.m_lck.Lock()
 	defer d.m_lck.Unlock()
 	uid := i.IC.Uid
+	if len(uid) < 1 {
+		return -1
+	}
 	if d.Res[uid] == nil {
 		d.Res[uid] = map[string]interface{}{}
 	}
