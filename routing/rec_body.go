320e4bdcd66010eef8ada15990a20e453af0ae12
Centny <centny@gmail.com>
2016-08-09 18:26:59 +0800

    adding xml return.

--- routing/rec_body.go
+++ routing/rec_body.go
@@ -2,18 +2,40 @@ package routing
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"github.com/Centny/gwf/util"
 	"io/ioutil"
 )
 
 func (h *HTTPSession) UnmarshalJ(v interface{}) error {
+	_, err := h.UnmarshalJ_v(v)
+	return err
+}
+
+func (h *HTTPSession) UnmarshalJ_v(v interface{}) ([]byte, error) {
 	bys, err := ioutil.ReadAll(h.R.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	err = json.Unmarshal(bys, v)
 	if err != nil {
-		return util.Err("Unmarshal json error(%v) for data:%v", err.Error(), string(bys))
+		return bys, util.Err("Unmarshal json error(%v) for data:%v", err.Error(), string(bys))
 	}
-	return nil
+	return bys, nil
+}
+
+func (h *HTTPSession) UnmarshalX(v interface{}) error {
+	_, err := h.UnmarshalX_v(v)
+	return err
+}
+func (h *HTTPSession) UnmarshalX_v(v interface{}) ([]byte, error) {
+	bys, err := ioutil.ReadAll(h.R.Body)
+	if err != nil {
+		return nil, err
+	}
+	err = xml.Unmarshal(bys, v)
+	if err != nil {
+		return bys, util.Err("Unmarshal xml error(%v) for data:%v", err.Error(), string(bys))
+	}
+	return bys, nil
 }
