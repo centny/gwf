2cf3a661dd98884bdb8749a77ee14bfd4d4f0db1
Centny <centny@gmail.com>
2016-10-21 14:38:45 +0800

    fix the last modified error.

--- routing/filter/nocache.go
+++ routing/filter/nocache.go
@@ -1,13 +1,14 @@
 package filter
 
 import (
-	"github.com/Centny/gwf/routing"
 	"time"
+
+	"github.com/Centny/gwf/routing"
 )
 
 func NoCacheFilter(hs *routing.HTTPSession) routing.HResult {
 	hs.W.Header().Set("Expires", "Tue, 01 Jan 1980 1:00:00 GMT")
-	hs.W.Header().Set("Last-Modified", time.Now().Format(time.RFC1123))
+	hs.W.Header().Set("Last-Modified", time.Now().UTC().Format("Mon, 02 Jan 2006 15:04:05 GMT"))
 	hs.W.Header().Set("Cache-Control", "no-stroe,no-cache,must-revalidate,post-check=0,pre-check=0")
 	hs.W.Header().Set("Pragma", "no-cache")
 	return routing.HRES_CONTINUE
