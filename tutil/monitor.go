8469de120564ae52cabdaeaf6d26966ab3cdb4c7
Centny <centny@gmail.com>
2017-09-25 10:04:20 +0800

    add concurrent monitor.

--- tutil/monitor.go
+++ tutil/monitor.go
@@ -2,12 +2,13 @@ package tutil
 
 import (
 	"fmt"
-	"github.com/Centny/gwf/util"
 	"math"
 	"path/filepath"
 	"sort"
 	"strings"
 	"sync"
+
+	"github.com/Centny/gwf/util"
 )
 
 type Statable interface {
@@ -15,16 +16,22 @@ type Statable interface {
 }
 
 type State struct {
-	Name  string `json:"name"`
-	Min   int64  `json:"min"`
-	Max   int64  `json:"max"`
-	Total int64  `json:"total"`
-	Count int64  `json:"count"`
+	Name    string `json:"name"`
+	Min     int64  `json:"min"`
+	Max     int64  `json:"max"`
+	Total   int64  `json:"total"`
+	Count   int64  `json:"count"`
+	ConcMax int64  `json:"con_max"`
+	ConcAvg int64  `json:"con_avg"`
+	//
+	concAll   uint64
+	concCount uint64
 }
 
 type Monitor struct {
 	Used     map[string]*State
 	Pending  map[string]int64
+	max      map[string]int64
 	lck      sync.RWMutex
 	sequence uint64
 }
@@ -33,6 +40,7 @@ func NewMonitor() *Monitor {
 	return &Monitor{
 		Used:    map[string]*State{},
 		Pending: map[string]int64{},
+		max:     map[string]int64{},
 		lck:     sync.RWMutex{},
 	}
 }
@@ -42,6 +50,17 @@ func (m *Monitor) Start(name string) string {
 	m.sequence += 1
 	var id = fmt.Sprintf("%v/%v", name, m.sequence)
 	m.Pending[id] = util.Now()
+	m.max[name]++
+	old, ok := m.Used[name]
+	if !ok {
+		old = &State{Name: name, Min: math.MaxInt64}
+	}
+	old.concAll += uint64(m.max[name])
+	old.concCount++
+	old.ConcAvg = int64(old.concAll / old.concCount)
+	if old.ConcMax < m.max[name] {
+		old.ConcMax = m.max[name]
+	}
 	return id
 }
 
@@ -75,6 +94,7 @@ func (m *Monitor) Done(id string) {
 		old.Min = used
 	}
 	m.Used[name] = old
+	m.max[name]--
 }
 
 func (m *Monitor) State() (interface{}, error) {
@@ -83,12 +103,14 @@ func (m *Monitor) State() (interface{}, error) {
 	var used = []util.Map{}
 	for _, u := range m.Used {
 		used = append(used, util.Map{
-			"name":  u.Name,
-			"min":   u.Min,
-			"max":   u.Max,
-			"total": u.Total,
-			"count": u.Count,
-			"avg":   u.Total / u.Count,
+			"name":     u.Name,
+			"min":      u.Min,
+			"max":      u.Max,
+			"total":    u.Total,
+			"count":    u.Count,
+			"avg":      u.Total / u.Count,
+			"conc_max": u.ConcMax,
+			"conc_avg": u.ConcAvg,
 		})
 	}
 	sort.Sort(util.NewMapSorterV(used, "/avg", 0, true))
