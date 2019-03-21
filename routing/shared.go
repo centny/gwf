e7deb638b42f400db67b096125323f0d02878486
centny <centny@gmail.com>
2019-03-21 13:51:11 +0800

    add handle stop

--- routing/shared.go
+++ routing/shared.go
@@ -3,7 +3,11 @@ package routing
 import (
 	"net"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
 var Shared = NewSessionMux2("")
@@ -21,14 +25,47 @@ func HFunc(pattern string, h HandleFunc) {
 	Shared.HFunc(pattern, h)
 }
 
-func ListenAndServe(addr string) error {
+var Server *http.Server
+var Listner net.Listener
+
+func ListenAndServe(addr string) (err error) {
+	Server = &http.Server{Handler: Shared}
 	if strings.HasPrefix(addr, "/") {
-		unixListener, err := net.Listen("unix", addr)
-		if err != nil {
-			return err
+		Listner, err = net.Listen("unix", addr)
+	} else {
+		Server.Addr = addr
+		Listner, err = net.Listen("tcp", addr)
+		if err == nil {
+			Listner = &tcpKeepAliveListener{TCPListener: Listner.(*net.TCPListener)}
 		}
-		server := &http.Server{Handler: Shared}
-		return server.Serve(unixListener)
 	}
-	return http.ListenAndServe(addr, Shared)
+	if err != nil {
+		return
+	}
+	return Server.Serve(Listner)
+}
+
+type tcpKeepAliveListener struct {
+	*net.TCPListener
+}
+
+func (ln tcpKeepAliveListener) Accept() (net.Conn, error) {
+	tc, err := ln.AcceptTCP()
+	if err != nil {
+		return nil, err
+	}
+	tc.SetKeepAlive(true)
+	tc.SetKeepAlivePeriod(3 * time.Minute)
+	return tc, nil
+}
+
+func HandleSignal() error {
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	<-sigc
+	return Listner.Close()
 }
